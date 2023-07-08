d8eeab32539a919c47fc844828acab873b9d4c76
AmorimY <rafael.amorim.praia@gmail.com>
2023-07-08 11:32:40 -0300

    init

---
+++ Hipotenusa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hipotenusa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro cateto:");
+            double c1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo cateto:");
+            double c2 = Convert.ToDouble(Console.ReadLine());
+
+            double hipotenusa = Math.Sqrt(Math.Pow(c1, 2) + Math.Pow(c2, 2));
+
+            Console.WriteLine("A hipotenusa é " + hipotenusa);
+                
+
+        }
+    }
+}
